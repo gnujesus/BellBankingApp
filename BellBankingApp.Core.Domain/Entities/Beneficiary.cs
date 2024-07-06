9c294a08f93076031fcf71a456089e06ce5c3625
IamNestor <160444238+IamNestor@users.noreply.github.com>
2024-07-05 21:04:00 -0400

    Capa Domain

---
+++ BellBankingApp.Core.Domain/Entities/Beneficiary.cs
@@ -0,0 +1,19 @@
+﻿using BellBanking.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellBanking.Core.Domain.Entities
+{
+    public class Beneficiary : AuditableBaseEntity
+    {
+        public string ProductId { get; set; }
+        public string UserId { get; set; }
+
+        //Navigation properties
+        public Product? Product { get; set; }
+        //public User User { get; set; }
+    }
+}
---
+++ BellBankingApp.Core.Domain/Entities/Product.cs
@@ -0,0 +1,22 @@
+﻿using BellBanking.Core.Domain.Common;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellBanking.Core.Domain.Entities
+{
+    public class Product : AuditableBaseEntity
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Type { get; set; }
+
+
+        //Navigation properties
+        //public User User { get; set; }
+        public List<Beneficiary>? Beneficiaries { get; set; }
+    }
+}
+

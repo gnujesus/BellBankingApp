1514c8a57e96be410406755697128370a249e377
IamNestor <160444238+IamNestor@users.noreply.github.com>
2024-07-05 21:20:20 -0400

    Capa de Application and Persistence

---
+++ BellBankingApp.Core.Application/Interfaces/Repositories/ITransactionRepository.cs
@@ -0,0 +1,13 @@
+﻿using BellBanking.Core.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BellBanking.Core.Application.Interfaces.Repositories
+{
+    public interface ITransactionRepository : IGenericRepository<Transaction>
+    {
+    }
+}

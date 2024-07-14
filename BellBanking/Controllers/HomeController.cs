fab8a161a7c6a894ebb672f63bf5789419956b99
gnujesus <jotamartinezd@gmail.com>
2024-07-14 02:06:12 -0400

    added dynamic home, beneficiaries and transactions

--- BellBanking/Controllers/HomeController.cs
+++ BellBanking/Controllers/HomeController.cs
@@ -1,16 +1,43 @@
-﻿using BellBanking.Models;
+﻿using BellBankingApp.Core.Application.Interfaces.Services;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
-using System.Diagnostics;
+using BellBankingApp.Core.Application.Helpers;
+using BellBankingApp.Core.Application.DTOs.Account;
+using BellBankingApp.Core.Application.Enums;
 
 namespace BellBanking.Controllers
 {
     [Authorize]
     public class HomeController : Controller
     {
+        private readonly IUserService _userService;
+        private readonly IProductService _productService;
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly ITransactionService _transactionService;
 
-        public IActionResult Index()
+        public HomeController(ITransactionService transactionService, IHttpContextAccessor httpContextAccessor, IUserService userService, IProductService productService, IBeneficiaryService beneficiaryService)
         {
+            _userService = userService;
+            _productService = productService;
+            _httpContextAccessor = httpContextAccessor;
+            _transactionService = transactionService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var user = _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>(Roles.Customer.ToString()) ??
+                       _httpContextAccessor.HttpContext.Session.Get<AuthenticationResponse>(Roles.Admin.ToString());
+            
+            var allTransactions = await _transactionService.GetAll();
+            var userTransactions = allTransactions
+                .Where(t => t.UserId == user.Id)
+                .OrderByDescending(t => t.DateCreated)
+                .Take(4)
+                .ToList();
+
+
+            ViewBag.UserTransactions = userTransactions;
+
             return View();
         }
     }

e501e26bbb078922bc8aa25c1cc61a2f2f3725b6
gnujesus <jotamartinezd@gmail.com>
2024-07-15 18:12:11 -0400

    added crud views

---
+++ BellBanking/Controllers/ProductController.cs
@@ -0,0 +1,17 @@
+﻿using BellBankingApp.Core.Application.ViewModels.Product;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.BellBankingApp.Controllers
+{
+    public class ProductController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View(new List<ProductViewModel>());
+        }
+        public IActionResult Create()
+        {
+            return View(new SaveProductViewModel());
+        }
+    }
+}
